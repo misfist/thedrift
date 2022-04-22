ebdebe73e6089936c18433bc1d023b25e9aebb2e
Pea <pea@misfist.com>
2022-04-22 15:14:30 -0400

    WordPress coding standards

--- wp-content/themes/drift/page-templates/subscribe.php
+++ wp-content/themes/drift/page-templates/subscribe.php
@@ -2,12 +2,12 @@
 /* Template name: Subscribe page */
 get_header();
 
-$page_imageID = get_post_thumbnail_id($pageID);
-if ($page_imageID == "") {
-    $page_imageURL = wp_get_attachment_image_src($page_imageID, "full");
-    $page_imageURL = $page_imageURL[0];
+$page_imageID = get_post_thumbnail_id( $pageID );
+if ( $page_imageID == '' ) {
+	$page_imageURL = wp_get_attachment_image_src( $page_imageID, 'full' );
+	$page_imageURL = $page_imageURL[0];
 } else {
-    $page_imageURL = get_bloginfo('template_url')."/assets/images/subscribe-box-bg.png";
+	$page_imageURL = get_bloginfo( 'template_url' ) . '/assets/images/subscribe-box-bg.png';
 }
 ?>
 
@@ -20,9 +20,9 @@ if ($page_imageID == "") {
 
 </style>
 <div class="the-drift-logo-mb" style="display: none;">
-    <a href="<?php echo home_url(); ?>">
-      <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/05/Logo.png">
-    </a>
+	<a href="<?php echo home_url(); ?>">
+	  <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/05/Logo.png">
+	</a>
 </div>
 <section>
 	<div class="container-fluid">
@@ -32,10 +32,10 @@ if ($page_imageID == "") {
 			<div class="ab_part_linner kudossubscribe">
 
 			<?php
-				if( $form = get_post_meta( get_the_ID(), 'form_name', true ) ) {
-					echo do_shortcode( '[fullstripe_form name="' . $form . '" type="inline_subscription"]' );
-				}
-				?>
+			if ( $form = get_post_meta( get_the_ID(), 'form_name', true ) ) {
+				echo do_shortcode( '[fullstripe_form name="' . $form . '" type="inline_subscription"]' );
+			}
+			?>
 				
 			</div>
 		</div>
@@ -56,10 +56,11 @@ if ($page_imageID == "") {
 				 </h3>
 				</div>
 				<?php
-             while (have_posts()):the_post();
-                the_content();
-             endwhile;
-            ?>
+				while ( have_posts() ) :
+					the_post();
+					the_content();
+			 endwhile;
+				?>
 			</div>
 		</div>
 	</div>
@@ -70,10 +71,10 @@ if ($page_imageID == "") {
 
 			jQuery("#submit--MGJjOTB").on("click", function(){
 				setTimeout(function(){
-				        if(jQuery(".wpfs-form-message--correct").is(':visible') )
-				        {
-				        	jQuery("#submit--MGJjOTB").addClass("disableButton");
-				        }
+						if(jQuery(".wpfs-form-message--correct").is(':visible') )
+						{
+							jQuery("#submit--MGJjOTB").addClass("disableButton");
+						}
 				}, 6000);
 			});
 		jQuery("#wpfs-card-holder-name--MGJjOTB").change(function(){
@@ -84,7 +85,7 @@ if ($page_imageID == "") {
 	jQuery('.custom_1').html('<span class="constrained">One-year print & digital</span><strong>$50</strong>');
 	jQuery('.custom_2').html('<span class="constrained">One-year digital</span><strong>$30</strong>');
 	jQuery('.custom_4').html('<span class="constrained">Institutional print & digital</span><strong>$90</strong>');
-    jQuery('.custom_5').html('<span class="constrained">International print & digital</span><strong>$90</strong>');
+	jQuery('.custom_5').html('<span class="constrained">International print & digital</span><strong>$90</strong>');
 	jQuery('.custom_3').html('<span class="constrained">Lifetime digital</span><strong>$300</strong>');
 	//console.log(jQuery('.wpfs-form-check').find('.wpfs-form-check-label').html());
 	});
@@ -92,33 +93,33 @@ if ($page_imageID == "") {
 </script>
 <style type="text/css">
 /*.page-template-subscribe .kudossubscribe form.wpfs-form fieldset.wpfs-form-check-group {
-    background: url(<?php echo $page_imageURL; ?>) !important;
-    }*/
+	background: url(<?php echo $page_imageURL; ?>) !important;
+	}*/
 	#wpfs-billing-address-panel--MGJjOTB > div:last-child, #wpfs-billing-address-panel--MGJjOTB > div:nth-child(1)
 	{
 		display: none;
 	}
 	.kudossubscribe form .wpfs-form-actions a {
-    display: none;
+	display: none;
 }
 label[for="wpfs-card-holder-name--MGJjOTB"]::before {
-    content: "Full name";
-    font-size: 18px !important;
+	content: "Full name";
+	font-size: 18px !important;
 }
 /*label[for="wpfs-plan--MGJjOTB--1"]::before {
-    content: "Lifetime $300";
-    font-size: 18px !important;
+	content: "Lifetime $300";
+	font-size: 18px !important;
 }*/
 label[for="wpfs-billing-address-line-1--MGJjOTB"]::before {
-    content: "Billing address";
-    font-size: 18px !important;
+	content: "Billing address";
+	font-size: 18px !important;
 }
 label[for="wpfs-card-holder-name--MGJjOTB"], label[for="wpfs-billing-address-line-1--MGJjOTB"]{
-    font-size: 0px !important;
+	font-size: 0px !important;
 }
 label[for='wpfs-same-billing-and-shipping-address--MGJjOTB']:before {
-    background: #909090 !important;
-    box-shadow: none !important;
+	background: #909090 !important;
+	box-shadow: none !important;
 }
 </style>
 <?php
