a530e94eef200a42efa4c05ae52660636e21e166
Pea <pea@misfist.com>
2022-04-23 21:37:40 -0400

    WIP Modify to apply to both form types

--- wp-content/themes/drift/assets/js/wpfs-script.js
+++ wp-content/themes/drift/assets/js/wpfs-script.js
@@ -4,9 +4,41 @@
     /**
      * Wrap desciption text in span tag so we can style as designed
      */
-    let subscriptionNodes = $( '.wpfs-form-check-group .wpfs-form-check' );    
+    const form = $( '.wpfs-form' );
+    if( ! form ) {
+        return;
+    }
+
+    const formType = form.data( 'wpfs-form-type' );
+    const subscriptionNodes = $( '.wpfs-form-check-group .wpfs-form-check' );
+
+    console.log( formType );
 
     if( subscriptionNodes ) {
+        if( 'inline_subscription' === formType ) {
+            formatSubscriptions();
+        }
+        // subscriptionNodes.each( function( index, value ) {
+        //     let $input = $( this ).find( 'input' );
+        //     let $label = $( this ).find( '.wpfs-form-check-label' );
+        //     let $amount =  $input.data( 'wpfs-plan-amount' );
+        //     let $currency = $input.data( 'wpfs-currency' );
+        //     let $planName = $input.data( 'wpfs-plan-name' );
+
+        //     let amount = new Intl.NumberFormat( 'en-US', { 
+        //         style: 'currency', 
+        //         currency: $currency,
+        //         minimumFractionDigits: 0,
+        //         maximumFractionDigits: 2
+        //     } ).format( $amount );
+
+        //     let $description = '<span class="constrained">' + $planName + '</span>';
+        //     let $price = '<strong>' + amount + '</strong>';
+        //     $label.html( $description + $price );
+        // } );
+    }
+
+    function formatSubscriptions() {
         subscriptionNodes.each( function( index, value ) {
             let $input = $( this ).find( 'input' );
             let $label = $( this ).find( '.wpfs-form-check-label' );
@@ -26,5 +58,7 @@
             $label.html( $description + $price );
         } );
     }
+
+    function formPayments() {}
 
 })( jQuery );
