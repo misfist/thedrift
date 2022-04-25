1fd32275d9c994ef4fac44853c83897234a23c60
Pea <pea@misfist.com>
2022-04-25 19:39:07 -0400

    New donate template
    Same as subscribe except form shortcode is different

--- wp-content/themes/drift/page-templates/donate.php
+++ wp-content/themes/drift/page-templates/donate.php
@@ -3,95 +3,61 @@
 get_header();
 ?>
 
-<div class="the-drift-logo-mb" style="display: none;">
-    <a href="<?php echo home_url(); ?>">
-      <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/05/Logo.png">
-    </a>                     	   	 	       
-</div>
+<?php
+if( $image = get_the_post_thumbnail_url( get_the_ID(), 'full' ) ) :
+	?>
+	<style type="text/css">
+		.page-donate form.wpfs-form fieldset.wpfs-form-check-group {
+			background-image: url( <?php echo esc_url( $image ); ?> );
+		}
+	</style>
 
+	<?php
+endif; ?>
+
+<div class="the-drift-logo-mb" style="display: none;">
+	<a href="<?php echo home_url(); ?>">
+	  <img src="<?php echo home_url(); ?>/wp-content/uploads/2020/05/Logo.png">
+	</a>
+</div>
 <section>
 	<div class="container-fluid">
 	<div class="ab_part d-flex">
 
 		<div class="ab_part_l d-flex">
-			<div class="ab_part_linner">
-				<div class="ab_part_img donate_img">
-					<?php
-					if ( $image = get_the_post_thumbnail_url( get_the_ID(), 'full' ) ) :
-						?>
-						<img src="<?php echo esc_url( $image );?>">
+			<div class="ab_part_linner kudossubscribe">
 
-					<?php
-					endif;
-					?>
-					<div class="ab_part_img_abu">
-						<div class="ab_part_img_inner d-flex">
-						<!-- <div>
-							<input type="radio" name="same" id="ten" class="custom_radio_button" value="10" data-selectID="ui-id-1" checked>
-							<label for="ten"> $10</label>
-						</div>
-						<div>
-							<input type="radio" name="same" id="two"  class="custom_radio_button" value="25"  data-selectID="ui-id-2">
-							<label for="two"> $25</label>
-						</div>
-						<div>
-							<input type="radio" name="same" id="thr"  class="custom_radio_button" value="50" data-selectID="ui-id-3">
-							<label for="thr"> $50</label>
-						</div>
-						<div>
-							<input type="radio" name="same" id="four"  class="custom_radio_button" value="100" data-selectID="ui-id-4">
-							<label for="four"> $100</label>
-						</div>
-						<div>
-							<input type="radio" name="same" id="five"  class="custom_radio_button" value="250" data-selectID="ui-id-5">
-							<label for="five"> $250</label>
-						</div>
-						<div>
-							<input type="radio" name="same" id="six"  class="custom_radio_button" value="Other" data-selectID="ui-id-6">
-							<label for="six"> Other amount</label>							
-							<input type="text" id="other_amount" value="" onkeypress="javascript:return isNumber(event)" >
-						</div> -->
-						</div>
-					</div>
-				</div>
+			<?php
+			if ( $form = get_post_meta( get_the_ID(), 'form_name', true ) ) {
+				echo do_shortcode( '[fullstripe_form name="' . $form . '" type="inline_payment"]' );
+			}
+			?>
 				
-				
-
-				<div class="form01">
-					<?php
-					if ( $form = get_post_meta( get_the_ID(), 'form_name', true ) ) {
-						echo do_shortcode( '[fullstripe_form name="' . $form . '" type="inline_payment"]' );
-					}
-					?>
-				</div> 
 			</div>
 		</div>
 
-
 		<div class="ab_part_r donate-subscribe_txt">
 			<div class="contact01">
-
 				<div class="com_heading">
-					<h3 class="entry-title"><strong><?php the_title(); ?></strong>
-					<?php
-					if ( $subsitle = get_post_meta( get_the_ID(), 'subsitle', true ) ) {
-						echo "<span class='line_gray'>|</span> " . $subsitle;
-					}
-					?>
+					<h3 class="entry-title"><strong><?php the_title(); ?></strong>​
+						<?php
+						if ( $subsitle = get_post_meta( get_the_ID(), 'subsitle', true ) ) {
+							echo "<span class='line_gray'>|</span> " . $subsitle;
+						}
+						?>
 					</h3>
 				</div>
-
-			<?php 
-			 while(have_posts()):the_post();
-			 	the_content();
-			 endwhile;
-			?>		
-			
+				<?php
+				while ( have_posts() ) :
+					the_post();
+					the_content();
+				endwhile;
+				?>
 			</div>
 		</div>
 	</div>
 </div>
-</section> 
+</section>
 
-<?php 
+<?php
 get_footer();
