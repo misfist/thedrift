7f42ddc6caca03d1a398b0d10d186b3e0bd7a60d
Pea <pea@misfist.com>
2022-06-06 11:10:11 -0400

    Add back cpts and taxonomies to theme

--- wp-content/themes/drift/post-type.php
+++ wp-content/themes/drift/post-type.php
@@ -1,112 +1,150 @@
-<?php 
+<?php
 
 add_action( 'init', 'create_issues' );
 function create_issues() {
-  $labels = array(
+	$labels = array(
 
-    'name' => _x('Issues', 'post type general name'),
+		'name'               => _x( 'Issues', 'post type general name' ),
 
-    'singular_name' => _x('Issues', 'post type singular name'),
+		'singular_name'      => _x( 'Issues', 'post type singular name' ),
 
-    'add_new' => _x('Add New Issue', ''),
+		'add_new'            => _x( 'Add New Issue', '' ),
 
-    'add_new_item' => __('Add New Issue'),
+		'add_new_item'       => __( 'Add New Issue' ),
 
-    'edit_item' => __('Edit Issue'),
+		'edit_item'          => __( 'Edit Issue' ),
 
-    'new_item' => __('New Issue'),
+		'new_item'           => __( 'New Issue' ),
 
-    'view_item' => __('View Issue'),
+		'view_item'          => __( 'View Issue' ),
 
-    'search_items' => __('Search Issue'),
+		'search_items'       => __( 'Search Issue' ),
 
-    'not_found' =>  __('No  found Issue'),
+		'not_found'          => __( 'No  found Issue' ),
 
-    'not_found_in_trash' => __('No found Issue in Trash'),
+		'not_found_in_trash' => __( 'No found Issue in Trash' ),
 
-    'parent_item_colon' => ''
+		'parent_item_colon'  => '',
 
-  );
+	);
 
-  $supports = array('title','editor','excerpt','trackbacks','thumbnail','comments','revisions','author','page-attributes');
+	$supports = array( 'title', 'editor', 'excerpt', 'trackbacks', 'thumbnail', 'comments', 'revisions', 'author', 'page-attributes' );
 
-  
+	register_post_type(
+		'issue',
+		array(
 
-  register_post_type( 'issue', 
+			'labels'        => $labels,
 
-    array(
+			'menu_icon'     => 'dashicons-book-alt',
 
-            'labels' => $labels,
+			'public'        => true,
 
-            'public' => true,
+			'supports'      => $supports,
 
-            'supports' => $supports,
+			'hierarchical'  => true,
 
-        	  'hierarchical' => true,
+			'menu_position' => 10,
 
-            'menu_position'       => 10,
+			'rewrite'       => array(
+				'slug'       => 'issue',
+				'with_front' => false,
+			),
 
-        	  'rewrite' => array('slug' => 'issue', 'with_front' => false),
-
-         )
-
-  );
+		)
+	);
 
 }
 
 
-// Mention post type here 
+// Mention post type here
 
 add_action( 'init', 'create_mentions' );
 function create_mentions() {
-  $labels = array(
+	$labels = array(
 
-    'name' => _x('Mentions', 'post type general name'),
+		'name'               => _x( 'Mentions', 'post type general name' ),
 
-    'singular_name' => _x('Mentions', 'post type singular name'),
+		'singular_name'      => _x( 'Mentions', 'post type singular name' ),
 
-    'add_new' => _x('Add New Mention', ''),
+		'add_new'            => _x( 'Add New Mention', '' ),
 
-    'add_new_item' => __('Add New Mention'),
+		'add_new_item'       => __( 'Add New Mention' ),
 
-    'edit_item' => __('Edit Mention'),
+		'edit_item'          => __( 'Edit Mention' ),
 
-    'new_item' => __('New Mention'),
+		'new_item'           => __( 'New Mention' ),
 
-    'view_item' => __('View Mention'),
+		'view_item'          => __( 'View Mention' ),
 
-    'search_items' => __('Search Mention'),
+		'search_items'       => __( 'Search Mention' ),
 
-    'not_found' =>  __('No  found Mention'),
+		'not_found'          => __( 'No  found Mention' ),
 
-    'not_found_in_trash' => __('No found Mention in Trash'),
+		'not_found_in_trash' => __( 'No found Mention in Trash' ),
 
-    'parent_item_colon' => ''
+		'parent_item_colon'  => '',
 
-  );
+	);
 
-  $supports = array('title','editor','excerpt','trackbacks','thumbnail','comments','revisions','author','page-attributes');
+	$supports = array( 'title', 'editor', 'excerpt', 'trackbacks', 'thumbnail', 'comments', 'revisions', 'author', 'page-attributes' );
 
-  
+	register_post_type(
+		'mention',
+		array(
 
-  register_post_type( 'mention', 
+			'labels'        => $labels,
 
-    array(
+			'menu_icon'     => 'dashicons-format-status',
 
-            'labels' => $labels,
+			'public'        => true,
 
-            'public' => true,
+			'supports'      => $supports,
 
-            'supports' => $supports,
+			'hierarchical'  => true,
 
-            'hierarchical' => true,
+			'menu_position' => 10,
 
-            'menu_position'       => 10,
+			'rewrite'       => array(
+				'slug'       => 'mention',
+				'with_front' => false,
+			),
 
-            'rewrite' => array('slug' => 'mention', 'with_front' => false),
+		)
+	);
 
-         )
+}
 
-  );
+function create_topics_hierarchical_taxonomy() {
+	// Add new taxonomy, make it hierarchical like categories
+	// first do the translations part for GUI
 
+	$labels = array(
+		'name'              => _x( 'Authors', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Author', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search Authors' ),
+		'all_items'         => __( 'All Authors' ),
+		'parent_item'       => __( 'Parent Author' ),
+		'parent_item_colon' => __( 'Parent Author:' ),
+		'edit_item'         => __( 'Edit Author' ),
+		'update_item'       => __( 'Update Author' ),
+		'add_new_item'      => __( 'Add New Author' ),
+		'new_item_name'     => __( 'New Author Name' ),
+		'menu_name'         => __( 'Authors' ),
+	);
+
+	// Now register the taxonomy
+
+	register_taxonomy(
+		'authors',
+		array( 'post' ),
+		array(
+			'hierarchical'      => true,
+			'labels'            => $labels,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'query_var'         => true,
+			'rewrite'           => array( 'slug' => 'article_author' ),
+		)
+	);
 }
